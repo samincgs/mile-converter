8b817d370e45dd18140a986defcf75c42103d054
samin <samincgs@gmail.com>
2024-07-13 22:21:30 -0400

    mile converter now has new styles with bootstrap, app is complete

--- mile-converter/main.py
+++ mile-converter/main.py
@@ -1,8 +1,14 @@
 import tkinter as tk
-from tkinter import ttk
+# from tkinter import ttk
+import ttkbootstrap as ttk
 
+def convert():
+    mile_input = entry_int.get()
+    convert_mile = mile_input * 1.61
+    output_string.set(convert_mile)
+    
 #window
-window = tk.Tk()
+window = ttk.Window(themename='pulse')
 window.title('Mile Converter')
 window.geometry('300x150')
 
@@ -13,11 +19,17 @@ title_label.pack()
 
 #input field
 input_frame = ttk.Frame(master=window)
-entry = ttk.Entry(master=input_frame)
-button = ttk.Button(master=input_frame, text='Convert')
+entry_int = tk.IntVar()
+entry = ttk.Entry(master=input_frame, textvariable=entry_int)
+button = ttk.Button(master=input_frame, text='Convert', command=convert)
 entry.pack(side='left', padx=10)
 button.pack(side='left')
 input_frame.pack(pady=15)
+
+#output
+output_string = tk.StringVar()
+output_label = ttk.Label(master=window, font='Calibri 20', textvariable=output_string)
+output_label.pack(pady=5)
 
 #run 
 window.mainloop()
